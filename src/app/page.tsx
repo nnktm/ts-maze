f72c817cb9541b8a6b2da8998fdf8f872d773789
nnktm <s1f102402417@iniad.org>
2025-06-28 01:00:53 +0900

    ランダムで壁を設置し迷路を作るcreateMaze関数を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,32 @@ function initialMazeMap(width: number, height: number) {
   return mazeMap;
 }
 
+function createMaze(nowMaze: number[][]) {
+  const newMaze = structuredClone(nowMaze);
+  const pillar: number[][] = [];
+  const newWall: number[][] = [];
+
+  const pillarPositions = newMaze.flatMap((row, y) =>
+    row.map((cell, x) => (cell === 1 ? [y, x] : [])).filter((pos) => pos.length > 0),
+  );
+  pillar.push(...pillarPositions);
+  const wallPositions = pillar.map(([y, x]) => {
+    const directions = DIRECTIONS.map(([dy, dx]) => [y + dy, x + dx]);
+    const randomDirection = directions[Math.floor(Math.random() * directions.length)];
+    if (newMaze[randomDirection[0]]?.[randomDirection[1]] !== undefined) {
+      newMaze[randomDirection[0]][randomDirection[1]] = 1;
+    }
+    return [randomDirection[0], randomDirection[1]];
+  });
+  newWall.push(...wallPositions);
+  return newMaze;
+}
+
+function calcMaze(nowMaze: number[][]) {
+  const maze = structuredClone(nowMaze);
+  return maze;
+}
+
 export default function Home() {
   const [mazeSet, setMazeSet] = useState<{ width: number; height: number }>({
     width: 11,
@@ -30,12 +56,14 @@ export default function Home() {
   });
 
   const [inputSetting, setInputSetting] = useState<{
-    width: string;
-    height: string;
+    width: number;
+    height: number;
   }>({
-    width: '11',
-    height: '11',
+    width: 11,
+    height: 11,
   });
+
+  const [nowMaze, setNowMaze] = useState<number[][]>(initialMazeMap(mazeSet.width, mazeSet.height));
 
   const handleWidthBlur = (e: React.FocusEvent<HTMLInputElement>) => {
     let value = Number(e.target.value);
@@ -45,7 +73,8 @@ export default function Home() {
     }
     const adjustedValue = value % 2 === 0 ? value + 1 : value;
     setMazeSet((prev) => ({ ...prev, width: adjustedValue }));
-    setInputSetting((prev) => ({ ...prev, width: adjustedValue.toString() }));
+    setInputSetting((prev) => ({ ...prev, width: adjustedValue }));
+    setNowMaze(initialMazeMap(adjustedValue, mazeSet.height));
   };
 
   const handleHeightBlur = (e: React.FocusEvent<HTMLInputElement>) => {
@@ -57,10 +86,15 @@ export default function Home() {
     const adjustedValue = value % 2 === 0 ? value + 1 : value;
 
     setMazeSet((prev) => ({ ...prev, height: adjustedValue }));
-    setInputSetting((prev) => ({ ...prev, height: adjustedValue.toString() }));
+    setInputSetting((prev) => ({ ...prev, height: adjustedValue }));
+    setNowMaze(initialMazeMap(mazeSet.width, adjustedValue));
   };
 
-  const maze = initialMazeMap(mazeSet.width, mazeSet.height);
+  const maze = calcMaze(nowMaze);
+
+  const handleCreateMaze = () => {
+    setNowMaze(createMaze(nowMaze));
+  };
 
   return (
     <div className={styles.container}>
@@ -75,7 +109,7 @@ export default function Home() {
             max="99"
             value={inputSetting.width}
             onChange={(e) => {
-              setInputSetting((prev) => ({ ...prev, width: e.target.value }));
+              setInputSetting((prev) => ({ ...prev, width: Number(e.target.value) }));
             }}
             onBlur={(e) => handleWidthBlur(e)}
             className={styles.textBox}
@@ -91,7 +125,7 @@ export default function Home() {
             max="99"
             value={inputSetting.height}
             onChange={(e) => {
-              setInputSetting((prev) => ({ ...prev, height: e.target.value }));
+              setInputSetting((prev) => ({ ...prev, height: Number(e.target.value) }));
             }}
             onBlur={(e) => handleHeightBlur(e)}
             className={styles.textBox}
@@ -101,26 +135,39 @@ export default function Home() {
               <strong>幅と高さを更新</strong>
             </button>
           </div>
+          <div className={styles.customMazeItem}>
+            <button onClick={handleCreateMaze}>
+              <strong>迷路を作成</strong>
+            </button>
+          </div>
         </div>
       </div>
-      <div
-        className={styles.maze}
-        style={{
-          gridTemplateRows: `repeat(${mazeSet.height}, 30px)`,
-          gridTemplateColumns: `repeat(${mazeSet.width}, 30px)`,
-        }}
-      >
-        {maze.map((row, y) =>
-          row.map((col, x) => (
-            <div
-              key={`${x}-${y}`}
-              className={styles.cell}
-              style={{
-                backgroundColor: col === 0 ? 'white' : 'black',
-              }}
-            />
-          )),
-        )}
+      <div className={styles.game}>
+        <div className={styles.startPoint}>
+          <p>start</p>
+        </div>
+        <div
+          className={styles.maze}
+          style={{
+            gridTemplateRows: `repeat(${mazeSet.height}, 30px)`,
+            gridTemplateColumns: `repeat(${mazeSet.width}, 30px)`,
+          }}
+        >
+          {maze.map((row, y) =>
+            row.map((col, x) => (
+              <div
+                key={`${x}-${y}`}
+                className={styles.cell}
+                style={{
+                  backgroundColor: col === 0 ? 'white' : 'black',
+                }}
+              />
+            )),
+          )}
+        </div>
+        <div className={styles.goalPoint}>
+          <p>goal</p>
+        </div>
       </div>
     </div>
   );
